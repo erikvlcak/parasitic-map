a248d6b055c610d357b9fb5c65a9c38e32ad896f
erikvlcak <erikvlcak1@gmail.com>
2024-02-08 00:39:25 +0100

    Update tailwind.config.js and package.json, add MapChart component

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
+
+// import ReactTooltip from 'react-tooltip'
+
+const geoUrl = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'
+
+export default function MapChart() {
+  return (
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <MapChart/>
+  </React.StrictMode>
 )
