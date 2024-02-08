0dfa8cc0c6e0f773e9181e59cc1ef24c2cb9c1f3
erikvlcak <erikvlcak1@gmail.com>
2024-02-08 21:57:11 +0100

    Add Mapbox integration and update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
 
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+import Map, { Marker, Popup } from 'react-map-gl'
+import { useState } from 'react'
 
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-// import ReactTooltip from 'react-tooltip'
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoiZXJ6ayIsImEiOiJjbHNkbWYzaHYxMmdtMmtvNnMxdGVlYjZoIn0.ILGOPb2X82sHmejxLrNu4A' 
 
-const geoUrl = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'
+function Root() {
+  const [viewState, setViewState] = React.useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14,
+  })
+  const [showPopup, setShowPopup] = useState(true)
 
-export default function MapChart() {
   return (
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
+    <Map
+      {...viewState}
+      onMove={(evt) => setViewState(evt.viewState)}
+      style={{ width: '90vw', height: '90vh' }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={-122.4} latitude={37.8} color="red" draggable />
+      {showPopup && (
+        <Popup
+          longitude={-100}
+          latitude={40}
+          anchor="bottom"
+          onClose={() => setShowPopup(false)}
+        >
+          You are here
+        </Popup>
+      )}
+    </Map>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MapChart/>
-  </React.StrictMode>
+const root = createRoot(
+  document.body.appendChild(document.createElement('div'))
 )
+root.render(<Root />)
